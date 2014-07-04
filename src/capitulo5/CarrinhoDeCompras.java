04dcd03d49e001f943600de6ec562fea84346435
georgematos <georgemattos@gmail.com>
2014-07-04 16:01:30 -0300

    Criacao das classes CarrinhoDeCompras e Item do capitulo5

---
+++ src/capitulo5/CarrinhoDeCompras.java
@@ -0,0 +1,23 @@
+package capitulo5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+	
+	private List<Item> itens;
+	
+	public CarrinhoDeCompras() {
+		this.itens = new ArrayList<Item>();
+	}
+	
+	public void adiciona(Item item) {
+		this.itens.add(item);
+	}
+
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens);
+	}
+	
+}
