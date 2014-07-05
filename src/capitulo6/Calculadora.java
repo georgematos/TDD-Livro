44ffdb27308043b66657d26c30110421586a45cf
georgematos <georgemattos@gmail.com>
2014-07-05 15:51:06 -0300

    Criacao do novo pacote do capitulo 6 e as classes Calculadora e CalculadoraTest

---
+++ src/capitulo6/Calculadora.java
@@ -0,0 +1,9 @@
+package capitulo6;
+
+public class Calculadora {
+
+	public double soma(int a, int b) {
+		return a + b;
+	}
+	
+}
---
+++ src/capitulo6/CalculadoraTest.java
@@ -0,0 +1,24 @@
+package capitulo6;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void deveSomarUmMaisUm() {
+		assertEquals(2, new Calculadora().soma(1,1), 0.00001);
+	}
+	
+	@Test
+	public void deveSomarUmMaisDois() {
+		assertEquals(3, new Calculadora().soma(1,2), 0.00001);
+	}
+	
+	@Test
+	public void deveSomarDoisMaisDois() {
+		assertEquals(4, new Calculadora().soma(2,2), 0.00001);
+	}
+	
+}
