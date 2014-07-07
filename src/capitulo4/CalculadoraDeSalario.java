e59279a99ce56345d3e36725f9c1ddcd33077e57
nayalison <nayalison@gmail.com>
2014-07-07 07:28:27 -0300

    Refatoracao da Classe CalculadoraDeSalario e seu teste

--- src/capitulo4/CalculadoraDeSalario.java
+++ src/capitulo4/CalculadoraDeSalario.java
@@ -5,23 +5,29 @@ public class CalculadoraDeSalario {
 	public double calcularSalario(Funcionario funcionario) {
 		
 		if (funcionario.getCargo() == Cargo.DESENVOLVEDOR) {
-			
-			if(funcionario.getSalario() < 3000) {
-				return funcionario.getSalario() * 0.9; // desconto de 10%
-			}
-			return funcionario.getSalario() * 0.8; // desconto de 20%
+			return dezOuVintePorCentoDeDesconto(funcionario);
 		}
 		
 		if (funcionario.getCargo() == Cargo.DBA || funcionario.getCargo() == Cargo.TESTADOR) {
-			
-			if(funcionario.getSalario() < 1500) {
-				return funcionario.getSalario() * 0.85; // desconto de 15%
-			}
-			return funcionario.getSalario() * 0.75; // desconto de 25%
+			return quinzeOuVintePorCentoDeDesconto(funcionario);
 		}
 		
 		return 0;
 		
+	}
+
+	private double quinzeOuVintePorCentoDeDesconto(Funcionario funcionario) {
+		if(funcionario.getSalario() < 1500) {
+			return funcionario.getSalario() * 0.85; // desconto de 15%
+		}
+		return funcionario.getSalario() * 0.75; // desconto de 25%
+	}
+
+	private double dezOuVintePorCentoDeDesconto(Funcionario funcionario) {
+		if(funcionario.getSalario() < 3000) {
+			return funcionario.getSalario() * 0.9; // desconto de 10%
+		}
+		return funcionario.getSalario() * 0.8; // desconto de 20%
 	}
 	
 }
