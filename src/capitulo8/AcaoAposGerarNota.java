7b905f2db98f16b2c0055d4e4e3eb5090cfd9d75
George <george@nayalison-OptiPlex-7010.(none)>
2014-07-08 09:23:18 -0300

    Melhorando o Design da classe GeradorDeNotaFiscal.

---
+++ src/capitulo8/AcaoAposGerarNota.java
@@ -0,0 +1,7 @@
+package capitulo8;
+
+public interface AcaoAposGerarNota {
+
+	void executa(NotaFiscal nf);
+	
+}
--- src/capitulo8/SAP.java
+++ src/capitulo8/SAP.java
@@ -1,9 +1,14 @@
 package capitulo8;
 
-public class SAP {
+public class SAP implements AcaoAposGerarNota {
 
 	public void envia(NotaFiscal nf) {
 		// envia NF para o SAP
+	}
+
+	@Override
+	public void executa(NotaFiscal nf) {
+		envia(nf);
 	}
 	
 }
